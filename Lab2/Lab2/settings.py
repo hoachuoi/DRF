b3f1caccf6c0bb65fabd5f9e0bbf72e28701b583
@bacnx <bacnx@outfiz.com>
2024-10-01 14:28:00 +0700

    Done QuerySets: Making queries | QuerySet method reference | Lookup expressions

--- Lab2/Lab2/settings.py
+++ Lab2/Lab2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Lab2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lab2',
+        'USER': 'root',
+        'PASSWORD': 'bacdz2002',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
